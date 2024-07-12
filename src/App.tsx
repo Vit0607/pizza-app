a813c82fd29b2d6c6c6710de24028a82f0fd90f4
Vitaly Yurkin <jvn9405@gmail.com>
2024-07-12 11:13:33 +0300

    Типизация компонент

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+import Button from './components/Button/Button';
+
+function App() {
+    return;
+    <>
+        <Button c>Кнопка</Button>
+    </>;
+}
+
+export default App;
