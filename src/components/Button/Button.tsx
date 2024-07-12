d4fdd436016bf48d96a072df108ca86e45862192
Vitaly Yurkin <jvn9405@gmail.com>
2024-07-12 14:34:01 +0300

    Подготовка стилей проекта

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,13 @@
-import './Button.module.css';
+import styles from './Button.module.css';
 import { ButtonProps } from '../Button.props';
 import cn from 'classnames';
 
 function Button({ children, className, ...props }: ButtonProps) {
     return (
-        <button className={cn('button accent', className)} {...props}>
+        <button
+            className={cn(styles['button'], styles['accent'], className)}
+            {...props}
+        >
             {children}
         </button>
     );
