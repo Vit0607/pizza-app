207c4dda4176607dff7e823dcfeb32c2c13ea0ce
Vitaly Yurkin <jvn9405@gmail.com>
2024-07-13 22:42:34 +0300

    Link

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,6 @@
 import { MouseEvent, useState } from 'react';
 import Button from './components/Button/Button';
 import Input from './components/Input/Input';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { Menu } from './pages/Menu/Menu';
-import { Cart } from './pages/Cart/Cart';
-import { Error } from './pages/Error/Error';
-
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Menu />
-    },
-    {
-        path: '/cart',
-        element: <Cart />
-    },
-    {
-        path: '*',
-        element: <Error />
-    }
-]);
 
 function App() {
     const [counter, setCounter] = useState<number>(0);
@@ -35,11 +16,6 @@ function App() {
                 Кнопка
             </Button>
             <Input placeholder="Email" />
-            <div>
-                <a href="/">Меню</a>
-                <a href="/cart">Корзина</a>
-            </div>
-            <RouterProvider router={router} />
         </>
     );
 }
