cd93c31fbabaa643f8b073f7cd0acb8131ff9c17
Vitaly Yurkin <jvn9405@gmail.com>
2024-07-16 22:48:22 +0300

    Упражнение - Заголовок и поиск

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,3 +1,14 @@
+import Headling from '../../components/HeadLing/Headling';
+import Search from '../../components/Search/Search';
+import styles from './Menu.module.css';
+
 export function Menu() {
-    return <>Menu</>;
+    return (
+        <>
+            <div className={styles['head']}>
+                <Headling>Меню</Headling>
+                <Search placeholder="Введите блюдо или состав" />
+            </div>
+        </>
+    );
 }
