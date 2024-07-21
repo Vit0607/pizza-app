0ee8612299eb58443c905cf30cbfb8c50259e616
Vitaly Yurkin <jvn9405@gmail.com>
2024-07-21 13:54:25 +0300

    lazy

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -8,41 +8,43 @@ import axios, { AxiosError } from 'axios';
 import { MenuList } from './MenuList/MenuList';
 
 export function Menu() {
-    const [products, setProducts] = useState<Product[]>([]);
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-    const [error, setError] = useState<string | undefined>();
+  const [products, setProducts] = useState<Product[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | undefined>();
 
-    const getMenu = async () => {
-        try {
-            setIsLoading(true);
-            const { data } = await axios.get<Product[]>(`${PREFIX}/products`);
-            setProducts(data);
-            setIsLoading(false);
-        } catch (e) {
-            console.error(e);
-            if (e instanceof AxiosError) {
-                setError(e.message);
-            }
-            setIsLoading(false);
-            return;
-        }
-    };
+  const getMenu = async () => {
+    try {
+      setIsLoading(true);
+      const { data } = await axios.get<Product[]>(`${PREFIX}/products`);
+      setProducts(data);
+      setIsLoading(false);
+    } catch (e) {
+      console.error(e);
+      if (e instanceof AxiosError) {
+        setError(e.message);
+      }
+      setIsLoading(false);
+      return;
+    }
+  };
 
-    useEffect(() => {
-        getMenu();
-    }, []);
+  useEffect(() => {
+    getMenu();
+  }, []);
 
-    return (
-        <>
-            <div className={styles['head']}>
-                <Headling>Меню</Headling>
-                <Search placeholder="Введите блюдо или состав" />
-            </div>
-            <div>
-                {error && <>{error}</>}
-                {!isLoading && <MenuList products={products} />}
-                {isLoading && <>Загружаем продукты...</>}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className={styles['head']}>
+        <Headling>Меню</Headling>
+        <Search placeholder="Введите блюдо или состав" />
+      </div>
+      <div>
+        {error && <>{error}</>}
+        {!isLoading && <MenuList products={products} />}
+        {isLoading && <>Загружаем продукты...</>}
+      </div>
+    </>
+  );
 }
+
+export default Menu;
