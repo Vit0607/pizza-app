6b95367f5ef74d912fe86c82c0c9a1a325b5c87d
Vitaly Yurkin <jvn9405@gmail.com>
2024-07-22 22:27:08 +0300

    defer и Await

--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -1,8 +1,17 @@
-import { useLoaderData } from 'react-router-dom';
-import { Product } from '../../interfaces/product.interface';
+import { Await, useLoaderData } from 'react-router-dom';
+import { Product as Pr } from '../../interfaces/product.interface';
+import { Suspense } from 'react';
 
 export function Product() {
-  const data = useLoaderData() as Product;
+  const data = useLoaderData() as { data: Pr };
 
-  return <>Product - {data.name}</>;
+  return (
+    <>
+      <Suspense fallback={'Загружаю...'}>
+        <Await resolve={data.data}>
+          {({ data }: { data: Pr }) => <>Product - {data.name}</>}
+        </Await>
+      </Suspense>
+    </>
+  );
 }
